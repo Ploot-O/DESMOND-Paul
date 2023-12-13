33698f33d73c2e2bbe4b801bbc957cb89132e535
adamspera <adamspera@hotmail.com>
2023-12-13 18:45:10 -0500

    Refactor feed.html and click.js
    This commit refactors the feed.html and click.js files. It adds a data-toggle and data-target attribute to the templateDmail card in feed.html, allowing it to trigger the dmailModal. It also updates the modal title and content in click.js to display the sender, subject, and body of the clicked card.

--- js/click.js
+++ js/click.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
   $('.card').click(function() {
-    console.log('card clicked');
     var sender = $(this).find('.sender').text();
     var subject = $(this).find('.subject').text();
     var body = $(this).find('.body').text();
 
-    $('#modal-sender').text('Sender: ' + sender);
-    $('#modal-subject').text('Subject: ' + subject);
-    $('#modal-body').text('Body: ' + body);
-
-    $('#dmailModal').modal('show');
+    $('#dmailModalLabel').text('Dmail from: ' + sender);
+    $('#dmailModal .modal-body').html('<p>Subject: ' + subject + '</p><br><br><p>Body: ' + body + '</p>');
   });
 });
