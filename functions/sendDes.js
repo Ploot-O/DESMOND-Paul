890dd26eab5c3f266feace2730abdaea26ede2f0
adamspera <adamspera@hotmail.com>
2023-12-13 15:10:26 -0500

    Refactor sendDes function to simplify form data handling

--- functions/sendDes.js
+++ functions/sendDes.js
@@ -1,31 +1,27 @@
 export async function onRequest(context) {
   try {
     const request = context.request;
-    if (request.method === 'POST') {
-      const formData = await request.formData();
-      const body = Object.fromEntries(formData.entries());
+    const formData = await request.formData();
+    const body = Object.fromEntries(formData.entries());
 
-      const formBody = new URLSearchParams();
-      for (const property in body) {
-        formBody.append(property, body[property]);
-      }
-
-      return 'Success'
-
-      // const response = await fetch(`https://${body.receiver}/receiveDes`, {
-      //   method: 'POST',
-      //   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      //   body: formBody
-      // });
-
-      // if (response.ok) {
-      //   return new Response('Data sent successfully', { status: 200 });
-      // } else {
-      //   throw new Error('Failed to post data');
-      // }
-    } else {
-      throw new Error('Invalid request method');
+    const formBody = new URLSearchParams();
+    for (const property in body) {
+      formBody.append(property, body[property]);
     }
+
+    return formBody.toString();
+
+    // const response = await fetch(`https://${body.receiver}/receiveDes`, {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    //   body: formBody
+    // });
+
+    // if (response.ok) {
+    //   return new Response('Data sent successfully', { status: 200 });
+    // } else {
+    //   throw new Error('Failed to post data');
+    // }
   } catch (err) {
     const errorResponse = JSON.stringify({ error: err.message });
     return new Response(errorResponse, { status: 400, headers: { 'Content-Type': 'application/json' } });
