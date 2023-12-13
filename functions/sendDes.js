e71e50ce5acea3ba467322fd757097e306c63eb2
adamspera <adamspera@hotmail.com>
2023-12-13 16:42:31 -0500

    Update response object in sendDes.js

--- functions/sendDes.js
+++ functions/sendDes.js
@@ -22,7 +22,7 @@ export async function onRequest(context) {
     });
 
     // Return the response from the receiving api.
-    return new Response(JSON.stringify(response, { status: response.status }));
+    return new Response(JSON.stringify({ "response": response }, { status: response.status }));
 
   } catch (error) {
     return new Response(error.message || 'Unknown error', { status: 500 });
