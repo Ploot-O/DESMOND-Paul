9194781b546ffb15cfd19232c144759f61e4ebaa
adamspera <adamspera@hotmail.com>
2023-12-13 15:46:53 -0500

    Refactor onRequest function to handle form data

--- functions/sendDes.js
+++ functions/sendDes.js
@@ -1,4 +1,12 @@
 export async function onRequest(context) {
-  console.log(JSON.stringify(context.request));
-  return new Response(JSON.stringify(context.request), { status: 200 });
+  try {
+    const formData = await context.request.formData();
+    var data = {};
+    for (let pair of formData.entries()) {
+      data[pair[0]] = pair[1];
+    }
+    return new Response(JSON.stringify(data), { status: 200 });
+  } catch (error) {
+    return new Response(error.message || 'Unknown error', { status: 500 });
+  }
 }
