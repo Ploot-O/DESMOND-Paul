23cf01c4c43afdaace26751b2724cb77e4eaff00
adamspera <adamspera@hotmail.com>
2023-12-13 15:43:56 -0500

    Refactor sendDes.js to simplify code and improve readability

--- functions/sendDes.js
+++ functions/sendDes.js
@@ -1,27 +1,4 @@
 export async function onRequest(context) {
-  console.log('Request received');
-  console.log(context.request);
   console.log(JSON.stringify(context.request));
-  const request = context.request;
-  const formData = await request.formData();
-  const body = Object.fromEntries(formData.entries());
-
-  const formBody = new URLSearchParams();
-  for (const property in body) {
-    formBody.append(property, body[property]);
-  }
-
-  return 'Done'
-
-  // const response = await fetch(`https://${body.receiver}/receiveDes`, {
-  //   method: 'POST',
-  //   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //   body: formBody
-  // });
-
-  // if (response.ok) {
-  //   return new Response('Data sent successfully', { status: 200 });
-  // } else {
-  //   throw new Error('Failed to post data');
-  // }
+  return new Response(JSON.stringify(context.request), { status: 200 });
 }
