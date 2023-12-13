b988217c56d6c9297318fbf4e2db4f5d8e904293
adamspera <adamspera@hotmail.com>
2023-12-13 18:35:05 -0500

    Update AJAX requests and modal display in index.html and click.js

--- js/click.js
+++ js/click.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
   $('.card').click(function() {
     var sender = $(this).find('.sender').text();
-    var destination = $(this).find('.destination').text();
     var subject = $(this).find('.subject').text();
     var body = $(this).find('.body').text();
 
     $('#modal-sender').text('Sender: ' + sender);
-    $('#modal-destination').text('Destination: ' + destination);
     $('#modal-subject').text('Subject: ' + subject);
-    $('#modal-body').html(body);
+    $('#modal-body').text('Body: ' + body);
 
     $('#emailModal').modal('show');
   });
