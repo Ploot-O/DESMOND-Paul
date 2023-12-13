38a6845099eff2639b597c252a0f1588744eff60
adamspera <adamspera@hotmail.com>
2023-12-13 16:14:39 -0500

    Add ID field to database statement

--- functions/receiveDes.js
+++ functions/receiveDes.js
@@ -9,13 +9,19 @@ export async function onRequest(context) {
 
     console.log('Form data processed:', data);
 
+    // Check if sender, subject, and body exist in the data object
+    if (!data.sender || !data.subject || !data.body) {
+      console.log('Error: sender, subject, or body is missing in the form data');
+      return new Response('Error: sender, subject, or body is missing', { status: 400 });
+    }
+
     // Prepare the database statement
-    const ps = context.env.DB.prepare(`INSERT INTO dmails (sender, subject, body) VALUES (?, ?, ?)`);
+    const ps = context.env.DB.prepare(`INSERT INTO dmails (id, sender, subject, body) VALUES (?, ?, ?)`);
 
     console.log('Database statement prepared');
 
     // Execute the statement
-    await ps.run(data.sender, data.subject, data.body);
+    await ps.run(0, data.sender, data.subject, data.body);
 
     console.log('Database statement executed');
 
