809de931bfb158f9823abc9486bd7fc3e6d5051c
adamspera <adamspera@hotmail.com>
2023-12-13 11:44:50 -0500

    Refactor dmail card layout in index.html

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ desmondApp.controller('desmondController', function ($scope) {
       'Authorization': 'Bearer your_token_here'
     },
     success: function (response) {
-      // Object.entries converts the data object into an array of pairs
-      // Then I'm using a map to transform the pairs into an object with title and details properties
       $scope.dmails = response.results;
       $scope.$apply();
     },
