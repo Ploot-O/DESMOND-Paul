7d9bbfd74fdb95c285ec2957e2e9d8fa60c64f45
adamspera <adamspera@hotmail.com>
2023-12-13 16:33:50 -0500

    Refactor form submission and database insertion in receiveDes.js, and update response format in sendDes.js

--- functions/receiveDes.js
+++ functions/receiveDes.js
@@ -1,34 +1,20 @@
 export async function onRequest(context) {
-  try {
-    // Process and get data from the form submission.
-    const formData = await context.request.formData();
-    var data = {};
-    for (let pair of formData.entries()) {
-      data[pair[0]] = pair[1];
-    }
-
-    // Check if sender, subject, and body exist in the data object
-    if (!data.sender || !data.subject || !data.body) {
-      return new Response('Error: sender, subject, or body is missing', { status: 400 });
-    }
-
-    try {
-
-      // Prepare the database statement
-      const db_response = await context.env.DB.prepare(`INSERT INTO dmails (sender, subject, body) VALUES (?1, ?2, ?3)`)
-        .bind(data.sender, data.subject, data.body).run();
-
-    } catch (dbError) {
-
-      return new Response(dbError.message, { status: 500, headers: { 'Content-Type': 'application/json' } });
-
-    }
-
-    // Respond saying that the data was received and logged.
-    return new Response(JSON.stringify({ message: 'Received and Logged.' }), { status: 200, headers: { 'Content-Type': 'application/json' } });
-
-  } catch (err) {
-    // Respond with an error message if something went wrong.
-    return new Response(JSON.stringify({ error: err.message }), { status: 501, headers: { 'Content-Type': 'application/json' } });
+  // Process and get data from the form submission.
+  const formData = await context.request.formData();
+  var data = {};
+  for (let pair of formData.entries()) {
+    data[pair[0]] = pair[1];
   }
+
+  // Check if sender, subject, and body exist in the data object
+  if (!data.sender || !data.subject || !data.body) {
+    return new Response('Error: sender, subject, or body is missing', { status: 400 });
+  }
+
+  // Prepare the database statement
+  const db_response = await context.env.DB.prepare(`INSERT INTO dmails (sender, subject, body) VALUES (?1, ?2, ?3)`)
+    .bind(data.sender, data.subject, data.body).run();
+
+  // Respond saying that the data was received.
+  return new Response(JSON.stringify({ message: 'Received.' }), { status: 200, headers: { 'Content-Type': 'application/json' } });
 }
