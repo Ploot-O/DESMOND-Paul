e5de58babb05695918d720895aaf86ad8c0adebd
adamspera <adamspera@hotmail.com>
2023-12-13 17:25:14 -0500

    Refactor receiveDes.js to handle errors and improve database handling

--- functions/receiveDes.js
+++ functions/receiveDes.js
@@ -1,18 +1,27 @@
 export async function onRequest(context) {
-  // Await for the json body from the request.
-  const data = await context.request.json();
+  try {
+    // Await for the json body from the request.
+    const data = await context.request.json();
 
-  console.log(data);
+    console.log(data);
 
-  // Check if sender, subject, and body exist in the data object
-  if (!data.destination || !data.sender || !data.subject || !data.body) {
-    return new Response(JSON.stringify({ message: 'Error: sender, destination, subject, or body is missing.' }), { status: 400 });
+    // Check if sender, subject, and body exist in the data object
+    if (!data.destination || !data.sender || !data.subject || !data.body) {
+      return new Response(JSON.stringify({ message: 'Error: sender, destination, subject, or body is missing.' }), { status: 400 });
+    }
+
+    try {
+      await context.env.DB.prepare(`INSERT INTO dmails (sender, subject, body) VALUES (?1, ?2, ?3)`)
+        .bind(data.sender, data.subject, data.body).run();
+    } catch (dbError) {
+      console.log('Database error:', dbError.message);
+      return new Response(JSON.stringify({ message: dbError.message }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+    }
+
+    // Respond saying that the data was received.
+    return new Response(JSON.stringify({ message: 'Dmail processed.', echo: data }), { status: 200, headers: { 'Content-Type': 'application/json' } });
+
+  } catch (error) {
+    return new Response(JSON.stringify({ message: error.message || 'Unknown error' }), { status: 500, headers: { 'Content-Type': 'application/json' } });
   }
-
-  // Prepare the database statement
-  const db_response = await context.env.DB.prepare(`INSERT INTO dmails (sender, subject, body) VALUES (?1, ?2, ?3)`)
-    .bind(data.sender, data.subject, data.body).run();
-
-  // Respond saying that the data was received.
-  return new Response(JSON.stringify({ message: 'Received.', echo: data }), { status: 200, headers: { 'Content-Type': 'application/json' } });
 }
