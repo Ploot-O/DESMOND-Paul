32f84c0ad6befa91aa1285eefb0c96a30ff6ebee
adamspera <adamspera@hotmail.com>
2023-12-13 11:31:32 -0500

    Refactored AJAX request in app.js and updated ng-repeat in index.html

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 angular.module('desmondApp', [])
-.controller('desmondController', function() {
-  var scope = this;
+  .controller('desmondController', ['$scope', function ($scope) {
 
-  var token = 'your_token_here';
-  var settings = {
-    url: '/collectDes',
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + token
-    }
-  };
+    var token = 'your_token_here';
+    var settings = {
+      url: '/collectDes',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    };
 
-  $.ajax(settings)
-    .done(function(response) {
-      scope.dmails = response.results;
-    })
-    .fail(function(jqXHR, textStatus) {
-      console.error('Error occurred:', textStatus);
-    });
-});
+    $.ajax(settings)
+      .done(function (response) {
+        $scope.dmails = response.results;
+      })
+      .fail(function (jqXHR, textStatus) {
+        console.error('Error occurred:', textStatus);
+      });
+  }]);
