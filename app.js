faa3b40ca708b87b44d4c6174220cc071f6f7d23
adamspera <adamspera@hotmail.com>
2023-12-13 11:32:31 -0500

    Refactor AJAX request in app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 angular.module('desmondApp', [])
-  .controller('desmondController', ['$scope', function ($scope) {
+.controller('desmondController', function($scope) {
 
-    var token = 'your_token_here';
-    var settings = {
-      url: '/collectDes',
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-      }
-    };
+  var token = 'your_token_here';
+  var settings = {
+    url: '/collectDes',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token
+    }
+  };
 
-    $.ajax(settings)
-      .done(function (response) {
+  $.ajax(settings)
+    .done(function(response) {
+      $scope.$apply(function() {
         $scope.dmails = response.results;
-      })
-      .fail(function (jqXHR, textStatus) {
-        console.error('Error occurred:', textStatus);
       });
-  }]);
+    })
+    .fail(function(jqXHR, textStatus) {
+      console.error('Error occurred:', textStatus);
+    });
+});
