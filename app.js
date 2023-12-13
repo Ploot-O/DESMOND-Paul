c2cbeb258ddc92581df8723102d18787524c15fe
adamspera <adamspera@hotmail.com>
2023-12-13 11:35:40 -0500

    Refactor AJAX request in app.js

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-angular.module('desmondApp', [])
-.controller('desmondController', function($scope) {
-
-  var token = 'your_token_here';
-  var settings = {
+var desmondApp = angular.module('desmondApp', []);
+desmondApp.controller('desmondController', function ($scope) {
+  $.ajax({
     url: '/collectDes',
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + token
-    }
-  };
-
-  $.ajax(settings)
-    .done(function(response) {
-      $scope.$apply(function() {
-        $scope.dmails = response.results;
-      });
-    })
-    .fail(function(jqXHR, textStatus) {
+      'Authorization': 'Bearer your_token_here'
+    },
+    success: function (response) {
+      // Object.entries converts the data object into an array of pairs
+      // Then I'm using a map to transform the pairs into an object with title and details properties
+      $scope.dmails = response.results;
+      $scope.$apply();
+    },
+    error: function (jqXHR, textStatus) {
       console.error('Error occurred:', textStatus);
-    });
+    }
+  });
 });
